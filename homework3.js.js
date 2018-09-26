01355bfb0f042c3cb55520dcefccd89a62345b09
annatea <anna_abrahamyan@edu.aua.am>
2018-09-27 00:51:27 +0300

    first commit

---
+++ homework3.js.js
@@ -0,0 +1,112 @@
+const func1 = function(a, b, c, d) {
+    return ((a-c)*(b-d)-(a\(c+b));
+};
+
+const func2 = function () {
+    return 'Hello world';
+};
+
+
+const func3 = function (a,b,c,d) {
+   console.log((a+b)*c);
+};
+  
+
+const func4 = function (string1, string2, string3) {
+   if (string1.length > string2.length && string1.length > string3.length){
+      return (string1);
+}  else if (string2.length > string1.length && string2.length > string3.length){
+      return (string2);
+}   else {
+      return (string3);
+}
+
+
+
+const func5 = function (number1, number2)
+   if(number1 === number2) {
+     return 0;
+} else if (number1 > number2) {
+     return 1;
+}  else {
+     return -1;
+}
+
+
+
+const multiply = function (number1, number2) {
+  const multiply = number1 * number2;
+     return multiply;
+}
+   
+
+const divide = function (number1, number2) {
+  const divide = number1 \ number2;
+     return divide;
+}
+
+
+
+
+
+const triangleArea = function(base, height) {
+  const area = divide((multiply(base, height),2)
+};
+
+
+
+const numLength = function ('8b576k') {
+  const numLength = numLength.length;
+     return numLength;
+};
+
+
+const func10 = funnction(string1, string2, number1)
+  if((string1 + string2).length > number1) {
+   return 1;
+}
+  else{
+     return -1;
+};
+
+
+
+
+const runStuff = function(number1, number2, string1) {
+  if (string1 === "rectangle") {
+     return multiply(number1, number2);
+}
+  else if (string1 === "triangle") {
+    return area(number1, number2);
+}
+ else { 
+     return -1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
